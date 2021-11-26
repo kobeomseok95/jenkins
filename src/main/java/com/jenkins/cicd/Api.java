2e1f5fda01452a91e28817a237566cc2ed1a7cfe
kobeomseok95 <kobumssh@naver.com>
2021-11-26 18:05:53 +0900

    init

---
+++ src/main/java/com/jenkins/cicd/Api.java
@@ -0,0 +1,21 @@
+package com.jenkins.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class Api {
+
+    @GetMapping("/")
+    public String health() throws Exception {
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        String hostName = InetAddress.getLocalHost().getHostName();
+        /**
+         *  TODO
+         *      return 변경 > 단순 String에서 JSON으로 변경하기
+         */
+        return hostAddress + ", " + hostName;
+    }
+}
---
+++ src/main/java/com/jenkins/cicd/HostInfoDto.java
@@ -0,0 +1,14 @@
+package com.jenkins.cicd;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HostInfoDto {
+
+    private String hostName;
+    private String hostAddress;
+}
